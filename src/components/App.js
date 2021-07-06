bbc836c9ee57075ee9c9149d5fc265f11734eb9e
Ebraj <gurungebraj22@gmail.com>
2021-07-06 14:00:01 +0545

    Data fetched

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [results, setResults] = useState([]);
+  const getDatas = async () => {
+    const result = await axios.get("https://api.jikan.moe/v3/search/anime", {
+      params: {
+        q: "Naruto",
+      },
+    });
+    const obtainedResults = result.data.results;
+    // console.log(obtainedResults);
+    setResults(obtainedResults);
+  };
+  console.log(results);
   return (
     <div>
-      <h1>App</h1>
+      <button onClick={getDatas}>Click Me</button>
     </div>
   );
 };
